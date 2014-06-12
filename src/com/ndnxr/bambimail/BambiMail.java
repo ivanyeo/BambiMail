0fff69706ea7c233e8c8c297b92d0c5fb0bf256f
Ivan Yeo <ivanyeo@cs.ucla.edu>
2014-06-11 23:10:39 -0700

    Implement UI for ease of usage.

--- src/com/ndnxr/bambimail/BambiMail.java
+++ src/com/ndnxr/bambimail/BambiMail.java
@@ -1,9 +1,16 @@
 package com.ndnxr.bambimail;
 
-import com.ndnxr.bambi.mailtest.R;
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
@@ -11,8 +18,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
-public class BambiMail extends ActionBarActivity {
+import com.ndnxr.bambi.BambiLib;
+import com.ndnxr.bambi.BambiLib.TASK_TYPE;
+import com.ndnxr.bambi.BambiLib.URGENCY;
+import com.ndnxr.bambi.Email;
+import com.ndnxr.bambi.Task;
+import com.ndnxr.bambi.mailtest.R;
+
+public class BambiMail extends ActionBarActivity implements
+		DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +43,21 @@ public class BambiMail extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		// Create Spinner items
+		Spinner spinner = (Spinner) findViewById(R.id.urgencySpinner);
+		List<String> list = new ArrayList<String>();
+		list.add("Normal");
+		list.add("Schedule");
+		list.add("Urgent");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+			android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(dataAdapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
@@ -44,11 +74,56 @@ public class BambiMail extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void start_service(View view) {
-		Intent i = new Intent("com.example.servicetest.MyService");
-		i.putExtra("KEY1", "Value to be used by the service");
-		this.startService(i);
+
+	public void send_email(View v) {
+		// Create email with attachment
+		String attachment = null;
+		String filename = "smurf2.png";
+
+		if (this.getFileStreamPath(filename).exists()) {
+			attachment = this.getFileStreamPath(filename).getAbsolutePath();
+		}
+
+		Email email = new Email("cs246rocks@gmail.com", "cs202rocks",
+				"smtp.gmail.com", "465", "woot", "subject here", "message",
+				new String[] { "cs246rocks@gmail.com" },
+				new String[] { attachment });
+
+		// Create Deadline
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		calendar.add(Calendar.MINUTE, 1);
+
+		Date deadline = calendar.getTime();
+
+		// Create a task
+		Task task = new Task(TASK_TYPE.EMAIL, URGENCY.SCHEDULE, deadline, email);
+
+		// Create instance of BambiLib
+		BambiLib bambiLib = new BambiLib(this);
+
+		// Send email
+		boolean output = bambiLib.sendEmail(task);
+
+		// Shutdown Lib
+		bambiLib.shutdown();
+
+		G.Log("send_email() DONE!");
+	}
+
+	public void schedule_email(View v) {
+		// Intent intent = new Intent(this, ScheduleActivity.class);
+		// startActivity(intent);
+	}
+
+	public void showTimePickerDialog(View v) {
+		DialogFragment newFragment = new TimePickerFragment();
+		newFragment.show(getSupportFragmentManager(), "timePicker");
+	}
+
+	public void showDatePickerDialog(View v) {
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getSupportFragmentManager(), "datePicker");
 	}
 
 	/**
@@ -66,6 +141,31 @@ public class BambiMail extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+
+	@Override
+	public void onDateSet(DatePicker view, int year, int monthOfYear,
+			int dayOfMonth) {
+		// Local Variable
+		String month = "";
+
+		// Set button's date
+		Button dateButton = (Button) this.findViewById(R.id.dateButton);
+
+		// Get month
+		DateFormatSymbols dfs = new DateFormatSymbols();
+		String[] months = dfs.getMonths();
+		month = months[monthOfYear];
+
+		dateButton.setText(String.format("%s %d, %d", month, dayOfMonth, year));
+	}
+
+	@Override
+	public void onTimeSet(TimePicker arg0, int hourOfDay, int minute) {
+		// Set button's date
+		Button timeButton = (Button) this.findViewById(R.id.timeButton);
+		
+		timeButton.setText(String.format("%d:%d", hourOfDay, minute));
 	}
 
 }
